932a68be73d41e33d1e91db10293def3a9ca9547
LaetitiaRA <laetitia.raison-aubry1@etu.univ-lorraine.fr>
2021-02-11 22:59:35 +0100

    'Frontend/'

--- Frontend/src/App.svelte
+++ Frontend/src/App.svelte
@@ -1,81 +1,123 @@
+<script>
+	// import '../node_modules/materialize-css/dist/css/materialize.min.css';
+	// import '../node_modules/materialize-css/dist/js/materialize.min.js';
+
+	// import { Router, Route } from "svelte-routing";
+
+	// import Navbar from './layout/Navbar.svelte';
+	// import Home from './pages/Home.svelte';
+	// import Creation from './pages/Creation.svelte';
+	// import Profil from './pages/Profil.svelte';
+	// import Messagerie from './pages/Messagerie.svelte';
+	// import Resultats from './pages/Resultats.svelte';
+</script>
+
+
+
+<!-- <Router>
+	<Navbar/>
+		<div class="container">
+			<Route path="/" component={Home} />
+			<Route path="/Creation" component={Creation} />
+			<Route path="/Resultats" component={Resultats} />
+			<Route path="/Profil" component={Profil} />
+			<Route path="/Messagerie" component={Messagerie} />
+		</div>
+</Router> -->
+
 <head>
 	<link rel="preconnect" href="https://fonts.gstatic.com">
 	<link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@1,700&display=swap" rel="stylesheet">
 	<link rel="preconnect" href="https://fonts.gstatic.com">
 	<link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@1,700&family=Varta:wght@300&display=swap" rel="stylesheet">
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Varta&display=swap" rel="stylesheet">
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Varta:wght@400;700&display=swap" rel="stylesheet">
 </head>
-
-<script>
-</script>
 
 <body>
 	<main>
 		<!-- svelte-ignore a11y-img-redundant-alt -->
-		<img id='logo' src="images/logo.png" alt="logo image" />
-		<hr/>
 	</main>
-	<div style="width: 100%;">
-		<div id="split-gauche" style="float:left; width: 50%">
-			<h2 class="h2-split-gauche"> <i>Participez à des expériences en linguistique et faites avancer la recherche</i></h2>
-			<p class="p-center">Vous avez déjà un compte ?</p>
-		</div>
-
-
-		<div id="split-droit" style="float:right;">
-		</div>
-	 </div>
-	 <div style="clear:both"></div>
+	<div class="split left" style="width:30%">
+		<div style="text-align: left"><img id='logo' src="images/logo.png" alt="Logo"></div>
+		  	<div id="left-part">
+				<p id="accroche">Participez à des expériences en <b>linguistique</b> et faites avancer la <b>recherche</b></p>
+				<p style="position:absolute; top:40%"> Vous avez déjà un compte ? <a href='/' class="button"><b>Connexion</b></a></p>
+			</div>
+	  	</div>
+	  
+	  <div class="split right" style="width:70%">
+		  <h2>John Doe</h2>
+		  <p>Some text here too.</p>
+	  </div>
 </body>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-	.h2-split-gauche {
-		margin:center;
-		text-align: center;
-		color : #313131;
-		width:100%;
-		font-family: 'Raleway', sans-serif;
-		font-size: 36px;
-	}
-	p{
-		font-family: 'Raleway', sans-serif;
-		font-family: 'Varta', sans-serif;
-		font-size: 26px;
-	}
-
-	.p-center{
-		text-align: center;
-	}
-
-	hr {
-		display: block;
-		height: 1px;
-		border: 0;
-		border-top: 1px solid #313131;
-		margin: auto;
-		padding: 0;
-		width:1230px;
-		height:0.6px;
-	}
 
 	body {
 		width : 100%;
 		height:150%;
-		background-image: linear-gradient(to right, rgba(246, 185,59,0), rgba(246, 185,59,0.2), rgba(246, 185,59,1));
 	}
+
+	p{
+		font-family: 'Varta', sans-serif;
+		font-size: 16px;
+	}
+	
+/* Split the screen in half */
+	.split {
+		height: 100%;
+		width: 50%;
+		position: fixed;
+		z-index: 1;
+		top: 0;
+		overflow-x: hidden;
+		padding-top: 20px;
+	}
+
+	/* Control the left side */
+	.left {
+		left: 0;
+		background-color: #FFFFFF;
+		position: absolute;
+		text-align: center;
+		font-family: 'Varta', sans-serif;
+	}
+
+	#accroche{
+		font-size:26px;
+		width:85%;
+		margin:auto;
+		position:absolute;
+		top:20%;
+	}
+
 	#logo {
-		width:15%;
+		width:28%;
+		margin:left;
 	}
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	.button {
+		padding: 15px 32px;
+		font-size: 16px;
+		cursor: pointer;
+		color:#FFFFFF;
+		background: #F6B93B;
+		border-radius: 26px;
+		width: 133px;
+		height: 56px;
+		left: 394px;
+		top: 509px;
 	}
 
+	/* Control the right side */
+	.right {
+		right: 0;
+		background-color: #F6B93B;
+		position: absolute;
+		text-align: center;
+		font-family: 'Varta', sans-serif;
+	}
 </style>
