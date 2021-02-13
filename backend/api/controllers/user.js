e9bd2532608c0659b7ed130d020dca469f066a20
Adrien J <ajeanne25@gmail.com>
2021-02-13 15:25:21 +0100

    rename controller

---
+++ backend/api/controllers/user.js
@@ -0,0 +1,78 @@
+const bcrypt = require('bcrypt');
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+
+
+exports.signup = (req, res, next) => {
+  bcrypt.hash(req.body.password, 10).then(
+      (hash) => {
+          const user = new User({
+            pseudo: req.body.pseudo,
+            email: req.body.email,
+            password: hash, //Faire vérification MDP
+            dateNaissance: req.body.dateNaissance,
+            sexe: req.body.sexe,
+            laboratoire: req.body.laboratoire,
+            trouble: req.body.trouble,
+            niveauScolaireID: req.body.niveauScolaire,
+            langueMatID: req.body.langueMeternelle, //Liste d'id?
+            langueParleID: req.body.langueParle,
+            villeID: req.body.ville
+          });
+          user.save().then(
+              () => {
+                  res.status(201).json({
+                      message: 'User added successfully!'
+                  });
+              }
+          ).catch(
+              (error) => {
+                  res.status(500).json({
+                      error: error
+                  });
+              }
+          );
+      }
+  );
+};
+
+
+exports.login = (req, res, next) => {
+  User.findOne({ email: req.body.email }).then(
+      (user) => {
+          if (!user) {
+              return res.status(401).json({
+                  error: new Error('User not found!')
+              });
+          }
+          bcrypt.compare(req.body.password, user.password).then(
+              (valid) => {
+                  if (!valid) {
+                      return res.status(401).json({
+                          error: new Error('Incorrect password!')
+                      });
+                  }
+                  const token = jwt.sign({ userId: user._id },
+                      'FannyMarineFlorenceLisaEstefaniaMarusyaRebeccaAliceMarieHélèneJulietteSolangeBérengèreElsa',
+                      { expiresIn: '24h' });
+                  res.status(200).json({
+                      userId: user._id,
+                      token: token
+                  });
+              }
+          ).catch(
+              (error) => {
+                  res.status(500).json({
+                      error: error
+                  });
+              }
+          );
+      }
+  ).catch(
+      (error) => {
+          res.status(500).json({
+              error: error
+          });
+      }
+  );
+};
