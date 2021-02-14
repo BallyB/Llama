ec58805eac1009cdfdc9e79ea0ed5b78004acb89
LaetitiaRA <laetitia.raison-aubry1@etu.univ-lorraine.fr>
2021-02-14 20:04:21 +0100

    "layout design"

--- Frontend/src/pages/LandingPage.svelte
+++ Frontend/src/pages/LandingPage.svelte
@@ -15,26 +15,52 @@
     <div style="text-align: left"><img id='logo' src="images/logo.png" alt="Logo"></div>
     <div id="left-part">
         <p class="accroche">Participez à des expériences en <em>linguistique </em> et faites avancer la <em>recherche</em></p>
-        <p style="text-align:center; margin-top:10%"> Vous avez déjà un compte ? </p>
+        <p> Vous avez déjà un compte ? </p>
         <Modal>
             <ConnexionComponent />
         </Modal>
+        <p class="quote">"LE meilleur (seul) site pour participer à des expériences en linguistique !" Béatrice.</p>
     </div>
 </div>
 
 <div class="split right" style="width:70%">
-    <h2>John Doe</h2>
+    <h2>Rejoignez-nous <br/> <hr/> dès maintenant</h2>
     <p>Some text here too.</p>
+
 </div>
 </body>
 
 <style>
+    hr {
+    display: block;
+    height: 1px;
+    width: 70px;
+    border: 0;
+    border-top: 1px solid #FFF;
+    padding: 0;
+    }
 
     body {
         width : 100vw;
         height:100vh;
     }
 
+    p{
+        text-align:'center';
+        margin-top:10%;
+    }
+
+    h2{
+        color: #FFF;
+        font-size:36px;
+    }
+
+    .quote{
+        margin-top:30%;
+        font-size: 30px;
+        font-family: 'Nothing you could do';
+    }
+    
     /* Split the screen in half */
     .split {
         height: 100vh;
@@ -55,21 +81,16 @@
     }
     .accroche{
         font-size:26px;
-        /* position:absolute; */
         margin-left: 10%;
         margin-right:10%;
         margin-top : 15%;
-        /* width:85%; */
-        /* margin:auto; */
-        /* position:absolute; */
-        /* top:20%; */
     }
+
     .accroche em{
         font-size: 28px;
         font-family: "Nothing You Could Do", cursive;
         font-weight: bold;
     }
-
 
     #logo {
         width: 28%;
