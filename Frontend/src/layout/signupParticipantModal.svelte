444793a38a67c57bcc58d0c817e6ba7e17e7eb23
MorganClt <morgan.co@live.fr>
2021-02-16 18:57:43 +0100

    form creation

---
+++ Frontend/src/layout/signupParticipantModal.svelte
@@ -0,0 +1,39 @@
+<script>
+
+</script>
+
+<body>
+<div>
+    <h1>Inscription participant</h1>
+</div>
+
+<form>
+    <input type="text" placeholder="Prénom">
+    <input type="text" placeholder="Nom">
+    <label>
+        <input type="radio">
+        Homme
+        <input type="radio">
+        Femme
+    </label>
+
+</form>
+
+</body>
+
+<style>
+
+    form {
+        width: 400px;
+        margin: auto;
+    }
+
+    h1 {
+        font-size: 2rem;
+        text-align: left;
+        color: #F6B93B;
+        width: 400px;
+        margin: 0 auto 13px;
+    }
+
+</style>
