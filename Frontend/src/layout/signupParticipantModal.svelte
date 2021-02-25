3f50ec547d7f4fbbdc9223bba074ccdcce7373d8
MorganClt <morgan.co@live.fr>
2021-02-25 16:49:12 +0100

    request inscription

--- Frontend/src/layout/signupParticipantModal.svelte
+++ Frontend/src/layout/signupParticipantModal.svelte
@@ -1,4 +1,8 @@
 <script>
+    import auth from "../store/auth";
+    import axios from "axios";
+    import {push} from "svelte-spa-router";
+
     /*function validation(){
         const password = document.getElementById("password").value;
         const confirm_password = document.getElementById("confirm password").value;
@@ -22,6 +26,116 @@
     /*password.onchange = validatePassword;
     confirm_password.onkeyup = validatePassword; */
 
+
+    let userType = 'participant';
+    let userName;
+    let sex;
+    let birthdayDate;
+    let departements = [
+        {
+            label: 'Ain',
+            value: 0,
+        },
+        {
+            label: 'Aine',
+            value: 1,
+        },
+        {
+            label: 'Allier',
+            value: 2,
+        },
+        {
+            label: 'Hautes-Alpes',
+            value: 3,
+        },
+        {
+            label: 'Basses-Alpes',
+            value: 4,
+        },
+        {
+            label: 'Ardeche',
+            value: 5,
+        },
+    ];
+    let dept = [];
+    let disorder;
+    let possibleLanguages = [
+        {
+            label: 'Allemand',
+            value: 0,
+        },
+        {
+            label: 'Anglais',
+            value: 1,
+        },
+        {
+            label: 'Espagnol',
+            value: 2,
+        },
+        {
+            label: 'Français',
+            value: 3,
+        },
+    ];
+    let motherTongues = [];
+    let languages = [];
+    let schoolLevels = [
+        {
+            label: 'BAC',
+            value: 0,
+        },
+        {
+            label: 'BAC +1',
+            value: 1,
+        },
+        {
+            label: 'BAC +2',
+            value: 2,
+        },
+        {
+            label: 'BAC +3',
+            value: 3,
+        },
+        {
+            label: 'BAC +4',
+            value: 4,
+        },
+        {
+            label: 'BAC +5',
+            value: 5,
+        },
+    ];
+    let school = [];
+    let email;
+    let password;
+
+    async function signup(e) {
+        e.preventDefault();
+
+        try {
+            const res = await axios.post('http://localhost:3000/api/auth/signupParticipant', {
+                nickname: userName,
+                email: email,
+                password: password,
+                birthDate: birthdayDate,
+                sex: sex,
+                disorder: disorder,
+                schoolDegreeID: school,
+                MaternalLanguageID: motherTongues,
+                SpokenLanguageID: languages,
+                cityID: dept,
+            });
+            /*console.log(res)
+            const { token, participantId } = res.data;
+
+            auth.setAuth({ userType, participantId, token });*/
+            await push('/home');
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+
 </script>
 
 <body>
@@ -29,66 +143,59 @@
     <h1>Inscription participant</h1>
 </div>
 
-<form onsubmit=" return validation()">
+<form on:submit={signup}>
     <label>
-        <input id = "userName" required type="text" placeholder="Nom d'utilisateur">
+        <input id = "userName" required type = "text" bind:value={userName} placeholder="Nom d'utilisateur">
     </label>
 
-    <p class="sex">Êtes-vous</p>
-    <div>
-        <input required type = "radio" name = "sex" value = "male"> <label class = "sex" for = "male">Un homme</label>
 
-        <input required type = "radio" name = "sex" value = "female"><label class = "sex" for = "female">Une femme</label>
+    <p class="sex" >Êtes-vous</p>
+    <div required>
+        <input type = "radio" name = "sex" bind:group={sex}> <label class = "sex" for = "male">Un homme</label>
 
-        <input required type = "radio"  name = "sex" value = "other" ><label class = "sex" for = "other">Je ne souhaite pas l'indiquer</label>
+        <input type = "radio" name = "sex" bind:group={sex}><label class = "sex" for = "female">Une femme</label>
+
+        <input type = "radio" name = "sex" bind:group={sex}><label class = "sex" for = "other">Je ne souhaite pas l'indiquer</label>
     </div>
 
-    <input required type = "date" id = "start" min = "1950-01-01" max = "2020-12-31">
+    <input required type = "date" id = "start" bind:value={birthdayDate} min = "1950-01-01" max = "2020-12-31">
 
-    <select required id = "dept" name = "dept">
-        <option value="" disabled selected>Sélectionnez votre département de résidence</option>
-        <option value="ain">Ain</option>
-        <option value="aisne">Aisne</option>
-        <option value="allier">Allier</option>
-        <option value="hautesalpes">Hautes-Alpes</option>
-        <option value="bassesalpes">Basses-Alpes</option>
-        <option value="ardeche">Ardèche</option>
+    <select required id = "dept" name = "dept" bind:value={dept}>
+        <option value = "" disabled selected>Sélectionnez votre département de résidence</option>
+        {#each departements as level}
+            <option value={level.value}>{level.label}</option>
+        {/each}
     </select>
 
-    <p class="trouble">Souffrez-vous d un trouble du language ? (écrit ou parlé)</p>
+    <p class="trouble" required>Souffrez-vous d un trouble du language ? (écrit ou parlé)</p>
     <div>
-        <input required type="radio" name = "trouble" value = "Oui"><label class = "trouble" for = "Oui">Oui</label>
-        <input required type="radio" name = "trouble" value = "Non"><label class = "trouble" for = "Non">Non</label>
+        <input type="radio" name = "trouble" bind:group={disorder}><label class = "trouble" for = "Oui">Oui</label>
+        <input type="radio" name = "trouble" bind:group={disorder}><label class = "trouble" for = "Non">Non</label>
     </div>
 
-    <select required id = "motherTongue" name = "langue" multiple>
+    <select required id = "motherTongue" name = "langue" multiple bind:value={motherTongues}>
         <option value = "" disabled selected>Sélectionnez votre ou vos langue(s) maternelle(s)</option>
-        <option value = "allemand">Allemand</option>
-        <option value = "anglais">Anglais</option>
-        <option value = "espagnol">Espagnol</option>
-        <option value = "français">Français</option>
+        {#each possibleLanguages as level}
+            <option value ={level.value}>{level.label}</option>
+        {/each}
     </select>
 
-    <select id = "langue" name = "langue" multiple>
+    <select id = "langue" name = "langue" multiple bind:value={languages}>
         <option value = "" disabled selected>Sélectionnez la ou les autre(s) langue(s) que vous parlez</option>
-        <option value = "allemand">Allemand</option>
-        <option value = "anglais">Anglais</option>
-        <option value = "espagnol">Espagnol</option>
-        <option value = "français">Français</option>
+        {#each possibleLanguages as level}
+            <option value={level.value}>{level.label}</option>
+        {/each}
     </select>
 
-    <select required id = "scho" name = "scho" >
+    <select required id = "scho" name = "scho" bind:value={school}>
         <option value = "" disabled selected>Sélectionnez votre niveau scolaire</option>
-        <option value = "BAC">BAC</option>
-        <option value = "BAC +1">BAC+1</option>
-        <option value = "BAC +2">BAC+2</option>
-        <option value = "BAC +3">BAC+3</option>
-        <option value = "BAC +4">BAC+4</option>
-        <option value = "BAC +5">BAC+5</option>
+        {#each schoolLevels as level}
+            <option value={level.value}>{level.label}</option>
+        {/each}
     </select>
 
-    <input id = "email"  type = "email" name = "email" placeholder = "Adresse mail">
-    <input id = "password" type = "password" name = "password" placeholder = "Mot de passe">
+    <input id = "email"  type = "email"  name = "email" bind:value={email} placeholder = "Adresse mail">
+    <input id = "password" type = "password"  name = "password" bind:value={password} placeholder = "Mot de passe">
     <!--<input type = "password" id = "confirm password " placeholder = "Confirmation du mot de passe">-->
 
     <button onclick="validation" type="submit" name = "submit" value = "submit" class="primary-button">inscription participant</button>
@@ -117,7 +224,7 @@
         color:#313131;
     }
 
-    #userName, #dept, #motherTongue, #langue, #password, #email {
+    #userName, #dept, #motherTongue, #langue, #password, #email, #scho {
         background-color: #eee;
         width: 100%;
         box-sizing: border-box;
