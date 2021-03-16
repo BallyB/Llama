9d4a21968ab0313e677d11ff502b9de875e2407e
LaetitiaRA <laetitia.raison-aubry1@etu.univ-lorraine.fr>
2021-03-16 13:11:17 +0100

    landing-page update

--- Frontend/src/pages/LandingPage.svelte
+++ Frontend/src/pages/LandingPage.svelte
@@ -9,6 +9,7 @@
                 <div class="logo">
                     <img id="logo" class="logo" src="images/logo.png" alt="Logo" />
                 </div>
+                <div></div>
                 <div>
                     <p class="accroche">
                         Participez à des expériences en <em>linguistique </em> et
@@ -20,53 +21,59 @@
                     <Modal>
                         <ConnexionComponent />
                     </Modal>
+                    
                 </div>
+                
                 <div>
                     <p class="quote">
                         "LE meilleur (seul) site pour participer à des expériences
                         en linguistique !" Béatrice.
                     </p>
                 </div>
+                <div></div>
             </div>
             <div class="col-right">
                 <div class="header-part">
-                    <h2>
-                        Rejoignez-nous <br />
-                        <hr />
-                        dès maintenant
-                    </h2>
+                    <h2 class="header-top"> Rejoignez-nous</h2>
+                    <hr />
+                    <h2 class="header-bottom">dès maintenant</h2>
                 </div>
                 <div class="mid-part">
                     <div class="left-part">
-                        <div class="creation">
-                            <img id="creation" src="images/creation.png" alt="creation"/>
-                            <p>Créez vos expériences</p></div>
-                        <div class="participants">
-                            <img id="participants" src="images/participants.png" alt="participants"/>
-                            <p>Trouvez des participants</p>
+                        <div class="img-left">
+                            <div> <img id="creation" src="images/creation.png" alt="creation"/>  </div>
+                            <div> <img id="participants" src="images/participants.png" alt="participants"/> </div>
+                            <div> <img id="followup" src="images/followup.png" alt="followup"/> </div>
                         </div>
-                        <div class="followup">
-                            <img id="followup" src="images/followup.png" alt="followup"/>
-                            <p>Réalisez le suivi</p>
+                        <div class="txt-left">
+                            <div class="creation">
+                                <p>Créez vos expériences</p></div>
+                            <div class="participants">
+                                <p>Trouvez des participants</p>
+                            </div>
+                            <div class="followup">
+                                <p>Réalisez le suivi</p>
+                            </div>
                         </div>
+                        
                     </div>
                     <div class="right-part">
+                        <div class="txt-right">
                             <div class="participation">
-                                <img id="participation" src="images/participation.png" alt="participation"/>
                                 <p>Participez aux expériences</p></div>
                             <div class="results">
-                                <img id="results" src="images/results.png" alt="results"/>
                                 <p>Accédez aux résultats</p>
                             </div>
                             <div class="reward">
-                                <img id="reward" src="images/reward.png" alt="reward"/>
                                 <p>Soyez récompensés</p>
                             </div>
-                        <!-- <div class="bottom-part">
-                            
-                        </div> -->     
+                        </div>
+                        <div class="img-right">
+                            <div> <img id="participation" src="images/participation.png" alt="participation"/>  </div>
+                            <div> <img id="results" src="images/results.png" alt="results"/> </div>
+                            <div> <img id="reward" src="images/reward.png" alt="reward"/> </div>
+                        </div>
                     </div>
-                    
                 </div>
                 <div class="bottom-part">
                     <div class="signup-researchers">
@@ -94,14 +101,6 @@
             margin:0;
         }
 
-        
-        /* Clear floats after the columns */
-        /* .row:after {
-            content: "";
-            display: table;
-            clear: both;
-        }
-
         /* Control the left side */
         .col-left {
             width: 30%;
@@ -110,7 +109,7 @@
             font-family: "Varta", sans-serif;
             margin:0;
             display: flex;
-            justify-content: flex-start;
+            justify-content: space-between;
             flex-direction: column;
         }
         .logo{
@@ -118,26 +117,28 @@
         }
 
         #logo{
-            width: 28%;
+            width: 10vw;
         }
         .quote {
-            margin-top: 25%;
-            margin-left: 10%;
-            margin-right: 10%;
+            margin-left: 5vw;
+            margin-right: 5vw;
             font-size: 22px;
             font-family: "Nothing you could do";
         }
         .accroche {
             font-size: 26px;
-            margin-left: 10%;
-            margin-right: 10%;
-            margin-top: 15%;
+            margin-left: 5vw;
+            margin-right: 5vw;
+            /* margin-top: 15%; */
         }
         .accroche em {
             font-size: 26px;
             font-family: "Nothing You Could Do", cursive;
             font-weight: bold;
         }
+
+
+
 
 
         /* Control the right side */
@@ -152,18 +153,46 @@
             justify-content: space-around;
         }
         .header-part{
-            /* justify-content: center; */
+            display:flex;
+            flex-direction: column;
+            justify-content: center;
         }
+
         .mid-part {
             width:100%;
             display: flex;
-            justify-content: space-evenly;
+            justify-content: space-around;
+
         }
 
         .left-part{
             display: flex;
+            justify-content: flex-start;
+        }
+
+        .txt-left{
+            display: flex;
             flex-direction: column;
             align-items: flex-start;
+            justify-content: space-between;
+        }
+        .img-left{
+            display: flex;
+            flex-direction: column;
+            align-items: flex-start;
+            justify-content: space-between;
+        }
+        .txt-right{
+            display: flex;
+            flex-direction: column;
+            align-items: flex-end;
+            justify-content: space-between;
+        }
+        .img-right{
+            display: flex;
+            flex-direction: column;
+            align-items: flex-end;
+            justify-content: space-between;
         }
         #creation{
             width: 30%;
@@ -176,10 +205,9 @@
             align-items: center;
             font-weight: 600;
             color: #313131;
-            margin-bottom: 46px;
         }
         #participants{
-            width: 26%;
+            width: 30%;
             /* width: 3em; */
         }
         .participants{
@@ -189,10 +217,9 @@
             align-items: center;
             font-weight: 600;
             color: #313131;
-            margin-bottom: 40px;
         }
         #followup{
-            width: 34%;
+            width: 30%;
             /* width: 3em; */
         }
         .followup{
@@ -202,15 +229,14 @@
             align-items: center;
             font-weight: 600;
             color: #313131;
-            margin-bottom: 40px;
         }
         .right-part{
             display: flex;
-            flex-direction: column;
-            align-items: flex-start;
+            justify-content: flex-start;
         }
+
         #participation{
-            width: 26%;
+            width: 30%;
             /* width: 3em; */
         }
         .participation{
@@ -220,7 +246,6 @@
             align-items: center;
             font-weight: 600;
             color: #313131;
-            margin-bottom: 40px;
         }
         #results{
             width: 30%;
@@ -233,7 +258,6 @@
             align-items: center;
             font-weight: 600;
             color: #313131;
-            margin-bottom: 40px;
         }
         #reward{
             width: 30%;
@@ -246,19 +270,14 @@
             align-items: center;
             font-weight: 600;
             color: #313131;
-            margin-bottom: 40px;
         }
 
         .bottom-part{
             width:100%;
             display: flex;
-            flex-direction: row;
             justify-content: space-around;
         }
      
-       
-
-
         p {
             text-align: "center";
             /* margin-top: 10%; */
@@ -270,8 +289,8 @@
         }
         hr {
             display: block;
-            height: 1px;
-            width: 70px;
+            height: 0.5vh;
+            width: 7vw;
             border: 0;
             border-top: 1px solid #fff;
             padding: 0;
