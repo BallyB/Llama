f1dfe8f75dc5ea47ebceb7d074d906cdbe4e7122
LaetitiaRA <laetitia.raison-aubry1@etu.univ-lorraine.fr>
2021-03-17 22:30:43 +0100

    primary black button added

--- Frontend/src/layout/NavbarResearcher.svelte
+++ Frontend/src/layout/NavbarResearcher.svelte
@@ -1,112 +1,112 @@
 <script>
-  export let selectedHome;
-  export let selectedCreation;
-  export let selectedResults;
-</script>
-
-<!-- <nav>
-  <div class="nav-container shadow">
-    <ul class="nav">
-      <li><a href="/">Home</a></li>
-      <li><a href="/Creation">Création</a></li>
-      <li><a href="/Results">Résultats</a></li>
-      <li><a href="/Profile">Profil</a></li>
-      <li><a href="/Chat">Messagerie</a></li>
-    </ul>
-  </div>
-</nav> -->
-
-
-<nav class="shadow">
-  <div class="left-links">
-    <a class="logo" href="/"><img class="logo" src="images/llama.png" alt="logo" /></a>
-  </div>
-  <div class="centered-links">
-    <a class="links {selectedHome}" href="/">Accueil</a>
-    <a class="links {selectedCreation}" href="/"> Création</a>
-    <a class="links {selectedResults}" href="/"> Résultats</a>
-  </div>
-  <div class="right-links">
-    <a class="profile" href="/"><img class="profile" src="images/user.png" alt="profile" /></a>
-    <a class="chat" href="/"><img class="chat" src="images/email.png" alt="chat" /></a>
-  </div>
-</nav>
-
-<style>
-  nav {
-    display: flex;
-    position: fixed;
-    top: 0;
-    width: 95vw;
-    overflow: auto;
-    height: auto;
-    justify-content: space-between;
-  }
-  .logo {
-    height: 50px;
-    display: inline-block;
-  }
-  .chat,.profile {
-    height: 20px;
-    display: inline-block;
-  }
-  .profile{
-    padding-right: 14px;
-  }
-  .left-links {
-    display: flex;
-    align-items: center;
-    justify-content: space-around;
-  }
-  .centered-links {
-    /* display: flex; */
-    align-items: center;
-    justify-content: space-around;
-  }
-  .right-links {
-    display: flex;
-    align-items: center;
-    justify-content: space-around;
-  }
-  .links {
-    display: inline-block;
-    text-align: center;
-    padding: 12px 24px;
-    color: #313131;
-    text-decoration: none;
-    font-size: 18px;
-    font-family: "Varta", sans-serif;
-  }
-  .links:hover {
-    border-bottom: 4px solid #f6b93b;
-  }
-  .selected {
-    border-bottom: 4px solid #f6b93b;
-    font-weight: bolder;
-  }
-  .shadow {
-    background-color: #fff;
-    padding: 10px;
-    box-shadow: 0px 15px 10px -15px #313131;
-    z-index: 1000;
-  }
-  /* .nav {
-    overflow: hidden;
-    list-style: none;
-    margin: 0;
-    padding: 0;
-    justify-content: center;
-    text-align: center;
-  }
-  .nav li {
-    display: inline;
-  }
-  .nav a {
-    display: inline-block;
-    padding: 20px;
-    color: #313131;
-    text-decoration: none;
-    font-size: 16px;
-    font-family: "Varta", sans-serif;
-  } */
-</style>
+	export let selectedHome;
+	export let selectedCreation;
+	export let selectedResults;
+  </script>
+  
+  <!-- <nav>
+	<div class="nav-container shadow">
+	  <ul class="nav">
+		<li><a href="/">Home</a></li>
+		<li><a href="/Creation">Création</a></li>
+		<li><a href="/Results">Résultats</a></li>
+		<li><a href="/Profile">Profil</a></li>
+		<li><a href="/Chat">Messagerie</a></li>
+	  </ul>
+	</div>
+  </nav> -->
+  
+  
+  <nav class="shadow">
+	<div class="left-links">
+	  <a class="logo" href="/"><img class="logo" src="images/llama.png" alt="logo" /></a>
+	</div>
+	<div class="centered-links">
+	  <a class="links {selectedHome}" href="/">Accueil</a>
+	  <a class="links {selectedCreation}" href="/"> Création</a>
+	  <a class="links {selectedResults}" href="/"> Résultats</a>
+	</div>
+	<div class="right-links">
+	  <a class="profile" href="/"><img class="profile" src="images/user.png" alt="profile" /></a>
+	  <a class="chat" href="/"><img class="chat" src="images/email.png" alt="chat" /></a>
+	</div>
+  </nav>
+  
+  <style>
+	nav {
+	  display: flex;
+	  position: fixed;
+	  top: 0;
+	  width: 95vw;
+	  overflow: auto;
+	  height: auto;
+	  justify-content: space-between;
+	}
+	.logo {
+	  height: 50px;
+	  display: inline-block;
+	}
+	.chat,.profile {
+	  height: 20px;
+	  display: inline-block;
+	}
+	.profile{
+	  padding-right: 14px;
+	}
+	.left-links {
+	  display: flex;
+	  align-items: center;
+	  justify-content: space-around;
+	}
+	.centered-links {
+	  /* display: flex; */
+	  align-items: center;
+	  justify-content: space-around;
+	}
+	.right-links {
+	  display: flex;
+	  align-items: center;
+	  justify-content: space-around;
+	}
+	.links {
+	  display: inline-block;
+	  text-align: center;
+	  padding: 12px 24px;
+	  color: #313131;
+	  text-decoration: none;
+	  font-size: 18px;
+	  font-family: "Varta", sans-serif;
+	}
+	.links:hover {
+	  border-bottom: 4px solid #f6b93b;
+	}
+	.selected {
+	  border-bottom: 4px solid #f6b93b;
+	  font-weight: bolder;
+	}
+	.shadow {
+	  background-color: #fff;
+	  padding: 10px;
+	  box-shadow: 0px 15px 10px -15px #313131;
+	  z-index: 1000;
+	}
+	/* .nav {
+	  overflow: hidden;
+	  list-style: none;
+	  margin: 0;
+	  padding: 0;
+	  justify-content: center;
+	  text-align: center;
+	}
+	.nav li {
+	  display: inline;
+	}
+	.nav a {
+	  display: inline-block;
+	  padding: 20px;
+	  color: #313131;
+	  text-decoration: none;
+	  font-size: 16px;
+	  font-family: "Varta", sans-serif;
+	} */
+  </style>
--- Frontend/src/layout/SignupResearchersComponent.svelte
+++ Frontend/src/layout/SignupResearchersComponent.svelte
@@ -18,7 +18,7 @@
 </script>
 
 <section>
-    <button class = "primary-button" on:click={showPopup}><b>Inscription chercheur</b></button>
+    <button class = "primary-button-black" on:click={showPopup}><b>Inscription chercheur</b></button>
     <br/>
 
 
@@ -39,42 +39,14 @@
     section {
         padding-top: 0.5em;
     }
-
-    .primary-button {
-        font-weight: bolder;
-        background-color: black;
-        width: 115px;
-        height: 47px;
+    
+    /* .primary-button-black {
         display: block;
         margin: auto;
         width: 193px;
         height: 46px;
         left: 394px;
         top: 509px;
-        font-size: 14px;
-    }
-
-    .primary-button:hover {
-        background-color: #313131;
-    }
-
-    /* button {
-        font-size: 14px;
-        sliders: pointer;
-        color:#FFFFFF;
-        background: #313131;
-        border: none;
-        border-radius: 26px;
-        box-shadow: 0px 10px 13px -7px #313131;
-        width: 193px;
-        height: 46px;
-        left: 394px;
-        top: 509px;
-        transition: all 0.34s;
-    }
-
-    button:hover {
-        background-color: #202020;
     } */
 
     #state {
