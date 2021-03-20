011e468aecae9a2ed26e0618fadc672122293cd8
LaetitiaRA <laetitia.raison-aubry1@etu.univ-lorraine.fr>
2021-03-20 13:19:44 +0100

    signup researcher

--- Frontend/src/layout/SignupResearchersModal.svelte
+++ Frontend/src/layout/SignupResearchersModal.svelte
@@ -1,138 +1,95 @@
 <script>
     import Select from "svelte-select";
     import axios from "axios";
-    import {push} from "svelte-spa-router";
+    import { push } from "svelte-spa-router";
     import auth from "../store/auth";
-    let error_boolean = false;
-    let lab =[
+    
+    const lab =[
         {value: 'labo1', label: 'Laboratoire 1'},
         {value: 'labo2', label: 'Laboratoire 2'}
     ];
-
+        
     let userType = 'researcher';
     let firstname;
-    let surname;
+    let name;
     let sex;
+    let laboratory;
     let email;
     let password;
 
-    function validateMessageEmail(event) {
-        let textbox = event.target;
-        error_boolean = false;
-        if (textbox.value === '') {
-            textbox.setCustomValidity("Veuillez renseigner ce champ");
-        } else if (textbox.validity.typeMismatch){
-            error_boolean = true;
-            textbox.setCustomValidity('Adresse email non valide');
-        } else {
-            textbox.setCustomValidity('');
-        }
-        return true;
-    }
-
 	
-	// function validation(){
-	// 	confirmPassword.setCustomValidity("")
-	// 	if (password.value !== confirmPassword.value){
-	// 		  confirmPassword.setCustomValidity("Les mots de passe saisis ne correspondent pas");
-    // 		confirmPassword.reportValidity()
-	// 			return
-	// 	}         
-	// 	if (formSignup.checkValidity()){
-	// 			alert('Votre inscription a bien été prise en compte !')
-	// 	}
-	// }
-
-    function handleSubmit(event) {
-        console.log(event);
-        console.log(event.target);
-        console.log(event.target.email.value);
-        console.log(event.target.password.value);
-    }
-
     async function signup(e) {
         e.preventDefault();
 
         try {
-            const res = await axios.post('http://localhost:3000/api/auth/signupResearcher', {
+            const res = await axios.post('http://localhost:3000/api/auth/SignupResearcher', {
                 firstname: firstname,
-                name: surname,
+                name: name,
                 email: email,
                 password: password,
                 sex: sex,
-                laboratory: lab,
+                laboratory: laboratory,
             });
-            /*console.log(res)
-            const { token, participantId } = res.data;
+            console.log(res)
+            const { token, researcherId } = res.data;
 
-            auth.setAuth({ userType, participantId, token });
-            await push('/home');*/
+            auth.setAuth({ userType, researcherId, token });
+            await push('#/home-r');
         } catch (error) {
             console.log(error)
         }
     }
 
-    
-
-
-    
-
 </script>
   
     <h1> Inscription Chercheur </h1>
-    <form id= "formSignup">
+    <form id= "formSignup" on:submit={signup}>
 
-        <input id ="firstname" required type="text" bind:value={firstname} placeholder="Prénom">
+        <input id ="firstname" required type="text" class="input-type" bind:value={firstname} placeholder="Prénom">
         <br/>
 
-        <input id ="surname" required type="text" bind:value={surname} placeholder="Nom">
+        <input id ="surname" required type="text" class="input-type" bind:value={name} placeholder="Nom">
         <br/>
 
         <p class="sex" >Êtes-vous</p>
-            <div>
-                <input required type="radio" name="sex" bind:group={sex}><label>Un homme</label>
+        <div>
+            <input required type="radio" name="sex" bind:group={sex} value="M"><label>Un homme</label>
 
-                <input required type="radio" name="sex" bind:group={sex}><label>Une femme</label>
+            <input required type="radio" name="sex" bind:group={sex} value="F"><label>Une femme</label>
 
-                <input required type="radio" name="sex" bind:group={sex}><label>Je ne souhaite pas l'indiquer</label>
-            </div>
+            <input required type="radio" name="sex" bind:group={sex} value="Undefined"><label>Je ne souhaite pas l'indiquer</label>
+        </div>
         <br/>
 
-        <div class="container">
-                <div class='select-item'><Select items={lab} bind:value={lab} isMulti={false} placeholder="Sélectionnez votre laboratoire de rattachement"/></div>
-        </div>
-        
-
-
-
+            <div class='select-item'><Select items={lab} bind:value={laboratory} isMulti={false} placeholder="Sélectionnez votre laboratoire de rattachement"/></div>
         <!-- <select required id="lab" name="lab">
             <option value="" disabled selected>Sélectionnez votre laboratoire de rattachement</option>
             <option value="Labo1">Laboratoire 1</option>
         </select>   -->
         <br/>
-
         <label for="email" id="profEmail">Votre adresse email professionnelle nous permet de limiter l'accès aux chercheurs.</label>
-        <input required type = "email" name = "email" id = "email" placeholder = "Adresse email professionnelle valide" /> 
+        <input required id="email"  type="email"  name="email" bind:value={email} class="input-type" placeholder="Adresse mail professionnelle valide">
+        <br/>
+        <input required id="password" type="password"  name="password" bind:value={password}  class="input-type" placeholder="Mot de passe">
         <br/>
 
-        <input required type = "password" id = "password" placeholder = "Mot de passe (entre 8 et 50 caractères)" />
-        <br/>
+
         <!-- <input required type = "password" id = "confirmPassword" placeholder = "Confirmation du mot de passe" /> -->
         <!-- <br/> -->
 
         <input required type="checkbox" name = "accept" id="accept"> <label id = "terms" for = "accept"> En poursuivant je reconnais avoir pris connaissance des termes et conditions d'utilisation.</label>
         <br/>
-        <a href="#">Comment sont utilisées mes données ?</a>
+        <a href>Comment sont utilisées mes données ?</a>
         <br/>
 
-        <button type="submit" class="primary-button-black" style="margin-top: 25px">inscription chercheur</button>
+        <button type="submit" class="primary-button-black">Inscription chercheur</button>
 
         <br/>
     </form>
 
 <style>
     form {
-       width: 400px;
+       width: 450px;
        margin: auto;
    }
     h1 {
@@ -152,33 +109,28 @@
     }
 
     #terms{
-        font-size : 12px;
+        font-size : 14px;
         color : #313131;
     }
-    a {
-        color : #eb2f06;
-        text-decoration : underline;
-        font-size : 12px;
-    }
-    a:hover{
-        color : #b71540;
-        font-weight: bold;
-    }
-    #firstname, #surname, #password, #email {
-        background-color: #eee;
-        width: 100%;
-        box-sizing: border-box;
-        border: none;
-        border-radius: 6px;
-        padding: 12px 15px;
-        margin: 8px auto;
-    }
+    
     .sex{
         font-size : 14px;
         color:#313131;
     }
     #profEmail{
         color:#eb2f06;
-        font-size:11px;
+        font-size:14px;
     }
+
+    a {
+        cursor : pointer;
+        color : #eb2f06;
+        text-decoration : underline;
+        font-size : 14px;
+    }
+    a:hover{
+        color : #b71540;
+        font-weight: bold;
+    }
+
 </style>
