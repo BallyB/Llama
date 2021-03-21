b49369ae8ca2a0be1a0ee6180d78199ed4243f01
Adrien J <ajeanne25@gmail.com>
2021-03-21 20:20:36 +0100

    CRUD ok

--- backend/api/controllers/index.js
+++ backend/api/controllers/index.js
@@ -1,5 +1,7 @@
 const Language = require('../models/language');
 const Region = require('../models/region');
+const SchoolDegree = require('../models/schoolDegree');
+const Laboratory = require('../models/laboratory');
 
 
 exports.getAllLanguages = (req, res, next) => {
@@ -25,17 +27,17 @@ exports.getAllLanguages = (req, res, next) => {
     );
 }
 
-exports.getExperiment = (req, res, next) => {
-    Experiment.findOne({ _id: req.params.id }).then(
-        (experiment) => {
-            if (!experiment) {
+exports.getLanguage = (req, res, next) => {
+    Language.findOne({ _id: req.params.id }).then(
+        (language) => {
+            if (!language) {
                 return res.status(401).json({
-                    error: new Error('No experiment found!')
+                    error: new Error('No language found!')
                 });
             }
             else{
                 return res.status(200).json({
-                    data: experiment
+                    data: language
                 });
             }
         }
@@ -55,6 +57,30 @@ exports.getAllRegions = (req, res, next) => {
             if (!region) {
                 return res.status(200).json({
                     error: new Error('No regions found!')
+                });
+            }
+            else{
+                return res.status(200).json({
+                    data: region
+                });
+            }
+        }
+    ).catch(
+        (error) => {
+            res.status(500).json({
+                error: error
+            });
+        }
+    );
+}
+
+
+exports.getRegion = (req, res, next) => {
+    Region.findOne({ _id: req.params.id }).then(
+        (region) => {
+            if (!region) {
+                return res.status(401).json({
+                    error: new Error('No region found!')
                 });
             }
             else{
@@ -95,12 +121,58 @@ exports.getAllschoolDegrees = (req, res, next) => {
     );
 }
 
+exports.getSchoolDegree = (req, res, next) => {
+    SchoolDegree.findOne({ _id: req.params.id }).then(
+        (schooldegree) => {
+            if (!schooldegree) {
+                return res.status(401).json({
+                    error: new Error('No school degree found!')
+                });
+            }
+            else{
+                return res.status(200).json({
+                    data: schooldegree
+                });
+            }
+        }
+    ).catch(
+        (error) => {
+            res.status(500).json({
+                error: error
+            });
+        }
+    );
+}
+
 
 exports.getAllLaboratories = (req, res, next) => {
     Laboratory.find().then(
         (laboratory) => {
             if (!laboratory) {
                 return res.status(400).json({
+                    error: new Error('No laboratory found!')
+                });
+            }
+            else{
+                return res.status(200).json({
+                    data: laboratory
+                });
+            }
+        }
+    ).catch(
+        (error) => {
+            res.status(500).json({
+                error: error
+            });
+        }
+    );
+}
+
+exports.getLaboratory = (req, res, next) => {
+    Laboratory.findOne({ _id: req.params.id }).then(
+        (laboratory) => {
+            if (!laboratory) {
+                return res.status(401).json({
                     error: new Error('No laboratory found!')
                 });
             }
