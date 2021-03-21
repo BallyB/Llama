e7b9c4e538439f57469be216b8b2e5a4e33a8dd6
Adrien J <ajeanne25@gmail.com>
2021-03-21 16:52:05 +0100

    passation

--- backend/api/models/experimentAnswer.js
+++ backend/api/models/experimentAnswer.js
@@ -4,7 +4,10 @@ const uniqueValidator = require('mongoose-unique-validator');
 const experimentAnswerSchema = mongoose.Schema({
   participantID: { type: Number, required: true},
   experimentID: { type: Number, required: true},
+  contact: { type: Boolean, required: true},
+  notification: { type: Boolean, required: true},
   content: {type: String, required: true}
+
 });
 
 experimentAnswerSchema.plugin(uniqueValidator);
